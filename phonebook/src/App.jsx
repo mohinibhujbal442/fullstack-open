90a3105406c58b6dfaabc3e1a686369fdedc8514
Mohini <https://github.com/mohinibhujbal442>
2026-09-15 00:13:20 +0530

    complete phonebook application

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,51 +1,118 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [search, setSearch] = useState('')
-  const [countries, setCountries] = useState([])
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
-  const handleSearchChange = event => {
-    const value = event.target.value
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
-    setSearch(value)
+  const addPerson = event => {
+    event.preventDefault()
 
-    if (value === '') {
-      setCountries([])
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    axios
+      .post('http://localhost:3001/api/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+        setMessage(`Added ${response.data.name}`)
+
+        setTimeout(() => setMessage(null), 3000)
+      })
+      .catch(error => {
+        setMessage(error.response?.data?.error || 'Something went wrong')
+
+        setTimeout(() => setMessage(null), 3000)
+      })
+  }
+
+  const deletePerson = id => {
+    const person = persons.find(person => person.id === id)
+
+    if (!window.confirm(`Delete ${person.name}?`)) {
       return
     }
 
     axios
-      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
-      .then(response => {
-        const allCountries = response.data
+      .delete(`http://localhost:3001/api/persons/${id}`)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
+      .catch(error => {
+        setMessage(error.response?.data?.error || 'Something went wrong')
 
-        const filteredCountries = allCountries.filter(country =>
-          country.name.common
-            .toLowerCase()
-            .includes(value.toLowerCase())
-        )
-
-        setCountries(filteredCountries)
+        setTimeout(() => setMessage(null), 3000)
       })
   }
 
+  const handleNameChange = event => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = event => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
+      <h2>Phonebook</h2>
+
       <div>
-        find countries{' '}
-        <input value={search} onChange={handleSearchChange} />
+        filter shown with{' '}
+        <input value={filter} onChange={handleFilterChange} />
       </div>
 
-      {countries.length > 10 ? (
-        <p>Too many matches, specify another filter</p>
-      ) : (
-        countries.map(country => (
-          <div key={country.cca3}>
-            {country.name.common}
-          </div>
-        ))
-      )}
+      <h2>add a new</h2>
+
+      {message && <div>{message}</div>}
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+
+      {personsToShow.map(person => (
+        <div key={person.id}>
+          {person.name} {person.number}{' '}
+          <button onClick={() => deletePerson(person.id)}>
+            delete
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
--- phonebook/src/services/persons.js
+++ phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -21,9 +21,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default {
-  getAll,
-  create,
-  remove,
-  update
-}
+export default { getAll, create, remove, update }
